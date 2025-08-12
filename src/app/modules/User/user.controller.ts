9715a142ad3dd7f4859202289d4bbae48b025107
tuhin <tuhin153695@gmail.com>
2025-08-12 17:22:04 +0600

    response formatting and exception handling

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,12 +1,24 @@
 // controller: handle request, response
 
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-//   console.log(req.body);
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  //   console.log(req.body);
+  try {
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Failed to create admin",
+      error: error,
+    });
+  }
 };
 
 export const userController = {
