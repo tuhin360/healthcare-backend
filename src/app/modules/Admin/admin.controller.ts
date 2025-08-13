be1aafc77f96fca4a410dbc25174c8312a18e74a
tuhin <tuhin153695@gmail.com>
2025-08-13 11:15:48 +0600

    selecting valid data fields using the pick function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
+import pick from "../../../Shared/pick";
 
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
+    const filters = pick(req.query, [
+      "searchTerm",
+      "name",
+      "email",
+      "contactNumber",
+    ]);
+
     console.log(req.query);
-    const result = await AdminService.getAllFromDB(req.query);
+
+    const result = await AdminService.getAllFromDB(filters);
+
     res.status(200).json({
       success: true,
       message: "Admins fetched successfully",
@@ -13,12 +23,13 @@ const getAllFromDB = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: error?.name || "Failed to fetch admins",
+      message: (error as Error)?.name || "Failed to fetch admins",
       error: error,
     });
   }
 };
 
+// Export controller so it can be used in routes
 export const AdminController = {
   getAllFromDB,
 };
