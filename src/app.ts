798d2439fa85f557e58b70fb10be9bea52f54a5d
tuhin <tuhin153695@gmail.com>
2025-08-23 04:47:43 +0600

    managing not found routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import status from "http-status";
+import { error } from "console";
 
 const app: Application = express();
 app.use(cors());
@@ -17,5 +19,17 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/v1", router);
 
 app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(status.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found"
+    }
+    
+  });
+});
 
 export default app;
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,16 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import status from "http-status";
+import { Prisma } from "../../generated/prisma";
+
 
 const globalErrorHandler = (
-  error: Error,
+  error: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  let message = error.message || "Something went wrong";
+
+  // Prisma known errors
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === "P2025") {
+      message = "No record found with the given ID";
+    }
+  }
+
   res.status(status.INTERNAL_SERVER_ERROR).json({
     success: false,
-    message: error.message || "Something went wrong",
-    error: error,
+    message,
+    error: {
+      name: error.name,
+      code: error.code,
+      meta: error.meta,
+    },
   });
 };
 
