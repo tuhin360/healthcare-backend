8355ad453e618fc2abb91d463ee33a7d7822bb36
tuhin <tuhin153695@gmail.com>
2025-08-27 23:15:28 +0600

    uploading images to current working directory with multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -4,11 +4,14 @@ import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "../../../generated/prisma";
 
+import { fileUploader } from "../../../helpers/fileUploader";
+
 const router: Router = express.Router();
 
 router.post(
   "/",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single("file"),
   userController.createAdmin
 );
 
