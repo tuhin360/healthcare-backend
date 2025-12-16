62aea34da370431e95348d1eb85772a987280cb0
tuhin <tuhin153695@gmail.com>
2025-12-16 10:41:11 +0600

    enabling prisma logging

--- src/Shared/prisma.ts
+++ src/Shared/prisma.ts
@@ -1,5 +1,33 @@
 import { PrismaClient } from "../generated/prisma";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
 
+prisma.$on("query", (e) => {
+  console.log("------------------------------");
+  console.log("Query: " + e.query);
+  console.log("------------------------------");
+  console.log("Params: " + e.params);
+  console.log("------------------------------");
+  console.log("Duration: " + e.duration + "ms");
+  console.log("------------------------------");
+});
 export default prisma;
